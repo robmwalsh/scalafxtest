f9b40edaa13c911453479caa2c67607d76a1b62f
rober <robert.walsh@contextpc.com>
2018-04-28 13:39:10 +1000

    initial commit

---
+++ src/main/scala/Test.scala
@@ -0,0 +1,8 @@
+import org.apache.flink.streaming.api.scala._
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    var env = StreamExecutionEnvironment.getExecutionEnvironment
+    println("hi")
+  }
+}
